029da395786a54506a768398055693579efc6226
knutc <knutc.kodehode@gmail.com>
2022-09-14 14:21:31 +0200

    + header is responsive

--- src/Components/NavBarHeader.js
+++ src/Components/NavBarHeader.js
@@ -1,19 +1,59 @@
-import { Header, Logo, NavList, StyledLink } from "../ComponentsStyled/StyledHeader";
+import { Header, Logo, NavList, StyledLink, NavToggle } from "../ComponentsStyled/StyledHeader";
 import LogoImg from "../img/jobloop_logo_colored.png";
+import { useState, useRef } from "react";
 
 const NavBarHeader = () => {
+  const [active, setActive] = useState(false);
+
+  const menuToggle = () => {
+    setActive(!active);
+  };
+
+  const autoClose = () => {
+    setActive(false);
+  };
+
+  const menu = useRef(null);
+
+  const closeMenu = (e) => {
+    if (menu.current && active && !menu.current.contains(e.target)) {
+      setActive(false);
+    }
+  };
+
+  document.addEventListener("mousedown", closeMenu);
+
   return (
-    <Header>
+    <Header ref={menu}>
       <Logo src={LogoImg} alt="JobLoop Logo" />
-      <NavList>
-        <StyledLink to="/about">About</StyledLink>
-        <StyledLink to="/contact">Contact</StyledLink>
-        <StyledLink to="/success-stories">Success Stories</StyledLink>
-        <StyledLink to="/media">Media</StyledLink>
-        <StyledLink to="/partners">Partners</StyledLink>
-        <StyledLink to="/syllabus">Syllabus</StyledLink>
-        <StyledLink to="/available-interns">Available Inters</StyledLink>
+      <NavList active={active}>
+        <StyledLink onClick={autoClose} to="/about">
+          About
+        </StyledLink>
+        <StyledLink onClick={autoClose} to="/contact">
+          Contact
+        </StyledLink>
+        <StyledLink onClick={autoClose} to="/success-stories">
+          Success Stories
+        </StyledLink>
+        <StyledLink onClick={autoClose} to="/media">
+          Media
+        </StyledLink>
+        <StyledLink onClick={autoClose} to="/partners">
+          Partners
+        </StyledLink>
+        <StyledLink onClick={autoClose} to="/syllabus">
+          Syllabus
+        </StyledLink>
+        <StyledLink onClick={autoClose} to="/available-interns">
+          Available Inters
+        </StyledLink>
       </NavList>
+      <NavToggle active={active} onClick={menuToggle}>
+        <span className="lines line--1"></span>
+        <span className="lines line--2"></span>
+        <span className="lines line--3"></span>
+      </NavToggle>
     </Header>
   );
 };
--- src/ComponentsStyled/StyledHeader.js
+++ src/ComponentsStyled/StyledHeader.js
@@ -17,7 +17,21 @@ export const Logo = styled.img`
 
 export const NavList = styled.nav`
   display: flex;
+  flex-wrap: wrap;
   gap: 12px;
+  transition: 0.6s;
+  @media (max-width: 820px) {
+    flex-direction: column;
+    position: fixed;
+    top: 100px;
+    right: 0;
+    gap: 40px;
+    width: 50%;
+    height: 100vh;
+    padding-top: 40px;
+    background-color: #77c4b4;
+    transform: translateX(${(props) => (props.active ? "0" : "100%")});
+  }
 `;
 
 export const StyledLink = styled(Link)`
@@ -25,5 +39,50 @@ export const StyledLink = styled(Link)`
   text-decoration: none;
   &:hover {
     cursor: pointer;
+  }
+  @media (max-width: 820px) {
+    padding-inline: 20px;
+    text-align: center;
+    font-size: 20px;
+  }
+`;
+
+export const NavToggle = styled.div`
+  position: relative;
+  width: 3rem;
+  height: 3.5rem;
+  cursor: pointer;
+  align-self: center;
+  justify-self: center;
+  display: none;
+
+  .lines {
+    position: absolute;
+    background: #f0f0f0;
+    border-radius: 5px;
+    transition: 0.4s;
+    height: 3px;
+    width: 35px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .line--1 {
+    transform: translate(-50%, ${(props) => (props.active ? "0" : "10px")})
+      rotate(${(props) => (props.active ? "-45deg" : "0")});
+  }
+
+  .line--2 {
+    width: ${(props) => (props.active ? "0%" : "")};
+  }
+
+  .line--3 {
+    transform: translate(-50%, ${(props) => (props.active ? "0" : "-13px")})
+      rotate(${(props) => (props.active ? "45deg" : "0")});
+  }
+
+  @media (max-width: 820px) {
+    display: block;
   }
 `;
