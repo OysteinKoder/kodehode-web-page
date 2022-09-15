ee350c9c6273d79152125dda359183cb3a2408db
Sofie <sofie.kodehode@gmail.com>
2022-09-15 14:31:18 +0200

    added AboutPage

---
+++ src/Globalstyle/Globalstyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        font-family: "Roboto";
+        font-size: 0.98rem;
+        
+    };
+    h1, h2{
+        color: #77C4B4;
+        margin: 0;
+        
+    }
+
+    h1{
+        font-size: 2rem;
+    }
+    
+    
+`;
+
+export default GlobalStyle;
